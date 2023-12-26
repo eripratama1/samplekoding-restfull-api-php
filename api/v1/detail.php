9022dc1ec2c8c985a1fdbda3ba62095b203acbea
eripratama1 <eripratama90@gmail.com>
2023-12-26 21:53:37 +0800

    create endpoint show detail data

---
+++ api/v1/detail.php
@@ -0,0 +1,53 @@
+<?php
+
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../../config/database.php';
+include_once '../../includes/functions.php';
+
+$database = new Database();
+$db = $database->getConnection();
+$functions = new Functions($db);
+
+/**
+ * Kode berikut digunakan untuk mendapatkan nilai dari parameter 'id' yang ada dalam query string. kode ini 
+ * menggunakan operator ternary untuk mengecek apakah 'id' ada dalam query string. 
+ * Jika ada, nilai 'id' diambil. Jika tidak, maka skrip die() dijalankan, yang menghentikan akan eksekusi script 
+ */
+$id = isset($_GET['id']) ? $_GET['id']: die();
+
+/** 
+ * Memanggil fungsi detail dari file Functions.php dan 
+ * passing parameter yahg diperlukan
+ */
+$stmt = $functions->detail($id);
+$num = $stmt->rowCount();
+
+if ($num > 0) {
+    /**
+     * Mengambil satu baris hasil query dan menyimpannya dalam bentuk array asosiatif. 
+     * Dalam konteks ini, kita berasumsi bahwa query tersebut hanya mengembalikan satu baris
+     */
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    /** Mengekstrak nilai dari baris hasil ke dalam variabel terpisah, 
+     * seperti $name, $description, dan $price. 
+     * Ini membuat nilai-nilai ini dapat digunakan secara langsung dalam pembuatan array produk. */
+    extract($row);
+
+    /**
+     * Membuat array $product_item yang berisi detail produk yang diambil dari hasil query.
+     */
+    $product_item = array(
+        "name" => $name,
+        "description" => $description,
+        "price" => $price,
+    );
+    http_response_code(200);
+    echo json_encode($product_item);
+} else {
+    http_response_code(404);
+    echo json_encode(array("message" => "Product not found"));
+}
--- includes/functions.php
+++ includes/functions.php
@@ -23,7 +23,7 @@ class Functions
     {
         /**  
          * Query untuk menambahkan data baru ke table products
-        */
+         */
         $query = "INSERT INTO products SET name=:name, description=:description, price=:price";
 
         /**
@@ -55,8 +55,21 @@ class Functions
         return false;
     }
 
-    public function read($record_num,$records_per_page)
+    /**
+     * Method read untuk menampilkan data dari tabel products
+     * $record_num: Parameter ini menentukan nomor catatan atau baris dari mana query akan dimulai. 
+     * Misalnya, jika kita ingin halaman pertama dari hasil query, kita akan mengatur $record_num menjadi 0
+    
+       $records_per_page: Parameter ini menentukan jumlah catatan atau baris yang akan diambil dalam satu halaman. 
+       Misalnya, jika kita ingin menampilkan 10 produk per halaman, kita akan mengatur $records_per_page menjadi 10.
+
+     */
+    public function read($record_num, $records_per_page)
     {
+        /**
+         * LIMIT {$record_num},{$records_per_page}: Menetapkan batasan pada jumlah baris yang diambil. 
+         * Dimulai dari baris ke-$record_num, diambil sebanyak $records_per_page baris.
+         */
         $query = "SELECT * FROM products LIMIT {$record_num},{$records_per_page}";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
@@ -64,6 +77,9 @@ class Functions
         return $stmt;
     }
 
+    /** 
+     * Method count digunakan untuk menghitung total baris (jumlah data) dalam tabel produk
+     */
     public function count()
     {
         $query = "SELECT COUNT(*) as total_rows FROM products";
@@ -72,5 +88,22 @@ class Functions
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         return $row['total_rows'];
+    }
+
+    /**
+     * Method yang digunakan untuk mengambil detail produk berdasarkan ID
+     */
+    public function detail($id)
+    {
+        /**
+         * query SQL yang digunakan untuk mengambil semua kolom dari tabel products di mana kolom id sama dengan nilai 
+         * yang diberikan ($id). Hanya satu baris (LIMIT 0,1) yang akan diambil.
+         */
+        $query = "SELECT * FROM products WHERE id = ? LIMIT 0,1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+
+        return $stmt;
     }
 }
